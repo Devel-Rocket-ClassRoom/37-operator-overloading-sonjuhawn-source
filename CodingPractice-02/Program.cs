19149ff880b972c9567b7cf74f58faedf78fdec1
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-09 22:02:33 +0900

    골드 완료

--- CodingPractice-02/Program.cs
+++ CodingPractice-02/Program.cs
@@ -1,4 +1,113 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.ConstrainedExecution;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("'''");
+{
+    Celsius c = new Celsius(100);
+    Console.WriteLine(c);
+    Console.WriteLine((Fahrenheit)c);
+    
+}
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+{
+
+}
+Console.WriteLine("'''\n");
+
+struct Celsius
+{
+    public double cel;
+    public Celsius(double cel)
+    {
+        this.cel = cel;
+    }
+    public override string ToString()
+    {
+        return $"{cel}°C";
+    }
+}
+struct Fahrenheit
+{
+    public double fa;
+    public Fahrenheit(double fa)
+    {
+        this.fa = fa;
+    }
+    public static explicit operator Celsius(Fahrenheit f)
+    {
+        return new Celsius((f.fa - 32) * 5 / 9);
+    }
+    public static explicit operator Fahrenheit(Celsius c)
+    {
+        return new Fahrenheit((c.cel * 9 / 5 + 32));
+    }
+    public override string ToString()
+    {
+        return $"{fa}°F";
+    }
+}
+
+struct Vector3
+{
+    public double x;
+    public double y;
+    public double z;
+
+    public Vector3(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public static Vector3 operator +(Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.x + v1.y, v1.y + v1.y, v1.z + v1.z);
+    }
+    public static Vector3 operator -(Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.x - v1.y, v1.y - v1.y, v1.z - v1.z);
+    }
+    public static Vector3 operator *(Vector3 v1, float num)
+    {
+        return new Vector3(v1.x * num, v1.y * num, v1.z * num);
+    }
+    public static Vector3 operator *(float num, Vector3 v1)
+    {
+        return v1 * num;
+    }
+    public static Vector3 operator -(Vector3 v1)
+    {
+        return new Vector3(-v1.x, -v1.y, -v1.z);
+    }
+    public static bool operator ==(Vector3 v1, Vector3 v2)
+    {
+        return (v1.x == v2.x) && (v1.y == v2.y) && (v1.z == v2.z);
+    }
+    public static bool operator !=(Vector3 v1, Vector3 v2)
+    {
+        return (v1.x != v2.x) || (v1.y != v2.y) || (v1.z != v2.z);
+    }
+    public override bool Equals(object obj)
+    {
+        if(obj is Vector3 v1)
+        {
+            return this == v1;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y, z);
+    }
+    public override string ToString()
+    {
+        return $"({x}, {y}, {z})";
+    }
+}
--- GameCurrency/Program.cs
+++ GameCurrency/Program.cs
@@ -1,4 +1,145 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 // README.md를 읽고 코드를 작성하세요.
 Console.WriteLine("코드를 작성하세요.");
+
+GameCurrency wallet1 = new GameCurrency(3, 50);
+GameCurrency wallet2 = new GameCurrency(1, 80);
+
+Console.WriteLine($"지갑1: {wallet1}");
+Console.WriteLine($"지갑2: {wallet2}");
+
+GameCurrency sum = wallet1 + wallet2;
+Console.WriteLine($"합계: {sum}");
+
+GameCurrency diff = wallet2 - wallet1;
+Console.WriteLine($"차액: {diff}");
+
+Console.WriteLine($"지갑1 == 지갑2: {wallet1 == wallet2}");
+Console.WriteLine($"지갑1 != 지갑2: {wallet1 != wallet2}");
+Console.WriteLine($"지갑1 > 지갑2: {wallet1 > wallet2}");
+Console.WriteLine($"지갑1 < 지갑2: {wallet1 < wallet2}");
+
+GameCurrency wallet3 = new GameCurrency(0, 250);
+Console.WriteLine($"250S 정규화: {wallet3}");
+Console.WriteLine($"지갑1 총 Silver: {wallet1.GetTotalSilver()}");
+
+struct GameCurrency
+{
+    public int Gold;
+    public int Silver;
+    public GameCurrency(int Gold, int Silver)
+    {
+        if(Gold == 0 && Silver >= 100)
+        {
+            this.Gold = Silver / 100;
+            this.Silver = Silver % 100;
+        }
+        else
+        {
+            this.Gold = Gold;
+            this.Silver = Silver;
+        }
+    }
+
+    public static GameCurrency operator +(GameCurrency a, GameCurrency b)
+    {
+        if(a.Silver + b.Silver >= 100)
+        {
+            return new GameCurrency(a.Gold + b.Gold + 1, a.Silver + b.Silver - 100);
+        }
+        else
+        {
+            return new GameCurrency(a.Gold + b.Gold, a.Silver + b.Silver);
+        }
+    }
+    public static GameCurrency operator -(GameCurrency a, GameCurrency b)
+    {
+        if(a.Gold-b.Gold < 0)
+        {
+            return new GameCurrency(0, 0);
+        }
+        else
+        {
+            return new GameCurrency(a.Gold - b.Gold, a.Silver - b.Silver);
+        }
+    }
+    public static bool operator ==(GameCurrency a, GameCurrency b)
+    {
+        return a.Gold == b.Gold && a.Silver == b.Silver;
+    }
+    public static bool operator !=(GameCurrency a, GameCurrency b)
+    {
+        return !(a == b); 
+    }
+    public static bool operator <(GameCurrency a, GameCurrency b)
+    {
+        if(a.Gold < b.Gold)
+        {
+            return true;
+        }
+        else if(a.Gold == b.Gold)
+        {
+            if(a.Silver < b.Silver)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static bool operator >(GameCurrency a, GameCurrency b)
+    {
+        if (a.Gold > b.Gold)
+        {
+            return true;
+        }
+        else if (a.Gold == b.Gold)
+        {
+            if (a.Silver > b.Silver)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public override string ToString()
+    {
+        return $"{Gold}G {Silver}S";
+    }
+
+    public int GetTotalSilver()
+    {
+        return Gold *100 + Silver;
+    }
+    public override bool Equals(object obj)
+    {
+        if(obj is GameCurrency game)
+        {
+            return this == game;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Gold, Silver);
+    }
+
+}
